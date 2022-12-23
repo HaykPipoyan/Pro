218e74730312a935794c76bce09d25be1bf8f892
Гайк Пипоян <haykpipoyan@MacBook-Air-Gajk.local>
2022-12-23 20:45:14 +0400

    second commit

--- Pro.py
+++ Pro.py
@@ -2,14 +2,12 @@ import pygame
 import tkinter as tkr
 from tkinter.filedialog import askdirectory
 import os
-​
 music_player = tkr.Tk()
 music_player.title("My Music Player")
 music_player.geometry("450x350")
 directory = askdirectory()
 os.chdir(directory)
 song_list = os.listdir()
-​
 play_list = tkr.Listbox(music_player, font="Helvetica 12 bold", bg='yellow', selectmode=tkr.SINGLE)
 for item in song_list:
     pos = 0
@@ -17,7 +15,6 @@ for item in song_list:
     pos += 1
 pygame.init()
 pygame.mixer.init()
-​
 def play():
     pygame.mixer.music.load(play_list.get(tkr.ACTIVE))
     var.set(play_list.get(tkr.ACTIVE))
@@ -32,10 +29,8 @@ Button1 = tkr.Button(music_player, width=5, height=3, font="Helvetica 12 bold", 
 Button2 = tkr.Button(music_player, width=5, height=3, font="Helvetica 12 bold", text="STOP", command=stop, bg="red", fg="white")
 Button3 = tkr.Button(music_player, width=5, height=3, font="Helvetica 12 bold", text="PAUSE", command=pause, bg="purple", fg="white")
 Button4 = tkr.Button(music_player, width=5, height=3, font="Helvetica 12 bold", text="UNPAUSE", command=unpause, bg="orange", fg="white")
-​
 var = tkr.StringVar() 
 song_title = tkr.Label(music_player, font="Helvetica 12 bold", textvariable=var)
-​
 song_title.pack()
 Button1.pack(fill="x")
 Button2.pack(fill="x")
